0af3febf0ba478f1e57f21ae5de58d24476bb904
Phat C. Vo <vophat0607@gmail.com>
2023-11-03 11:38:01 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from mpc_cbf import MPC
+from plotter import Plotter
+import util
+
+
+def main():
+    np.random.seed(99)
+
+    # Define controller & run simulation
+    controller = MPC()
+    controller.run_simulation()  # Closed-loop control simulation
+
+    # Plots
+    plotter = Plotter(controller)
+    plotter.plot_results()
+    plotter.plot_predictions()
+    plotter.plot_path()
+    plotter.create_trajectories_animation()
+    plotter.create_path_animation()
+    plotter.plot_cbf()
+
+    # Store results
+    # util.save_mpc_results(controller)
+
+
+if __name__ == '__main__':
+    main()
